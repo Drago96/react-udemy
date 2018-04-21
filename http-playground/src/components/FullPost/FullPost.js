c68e70b7fc53b3aadfd1a367a696b5cb152e908a
Drago96 <dragproychev@gmail.com>
2018-04-21 22:11:07 +0300

    Finished up axios introduction

--- http-playground/src/components/FullPost/FullPost.js
+++ http-playground/src/components/FullPost/FullPost.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import axios from "axios";
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    static propTypes = {
+        id: PropTypes.number
+    }
 
-        );
+    state = {
+        loadedPost: null
+    }
+
+    async componentDidUpdate() {
+        if (this.props.id) {
+            if (!this.state.loadedPost || (this.state.loadedPost
+                && this.state.loadedPost.id !== this.props.id)) {
+                const { data } = await axios.get("/posts/" + this.props.id);
+                this.setState({ loadedPost: data });
+            }
+        }
+    }
+
+    deletePostHandler = async () => {
+        const response = await axios.delete("/posts/" + this.props.id);
+        console.log(response);
+    }
+
+    render() {
+        let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+
+        if (this.props.id) {
+            post = <p style={{ textAlign: "center" }}>Loading...</p>;
+        }
+
+        if (this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
+
         return post;
     }
 }
--- http-playground/src/containers/Blog/Blog.js
+++ http-playground/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "../../axios";
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,50 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    async componentDidMount() {
+        try {
+            const { data } = await axios.get("/posts");
+
+            const posts = data.slice(0, 4);
+            posts.forEach(p => {
+                p.author = "Drago";
+            });
+            this.setState({ posts });
+        } catch (e) {
+            this.setState({ error: true });
+        }
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
+
+        if (!this.state.error) {
+            posts = this.state.posts.map(p => (
+                <Post
+                    key={p.id}
+                    title={p.title}
+                    author={p.author}
+                    clicked={() => this.postSelectedHandler(p.id)} />
+            ));
+        }
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
