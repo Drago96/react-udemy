073b5482f08fb180603f9e170c3325ebfdf7fcf2
Drago96 <dragproychev@gmail.com>
2018-04-26 21:54:48 +0300

    Advanced redux concepts

---
+++ redux-playground/src/store/actions/counter.js
@@ -0,0 +1,29 @@
+import { INCREMENT, DECREMENT, ADD, SUBTRACT } from "./actionTypes";
+
+export const increment = () => {
+    return {
+        type: INCREMENT
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: DECREMENT
+    };
+};
+
+
+export const add = (value) => {
+    return {
+        type: ADD,
+        value
+    };
+};
+
+
+export const subtract = (value) => {
+    return {
+        type: SUBTRACT,
+        value
+    };
+};
--- redux-playground/src/store/reducers/result.js
+++ redux-playground/src/store/reducers/result.js
@@ -1,4 +1,5 @@
-import * as actionTypes from "../actions";
+import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
     results: []
@@ -7,27 +8,31 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: [...state.results, {
-                    id: new Date(),
-                    value: action.result
-                }]
-            };
+            return storeResult(state, action);
 
         case actionTypes.DELETE_RESULT:
-            return {
-                ...state,
-                results: [
-                    ...state.results.filter(el => {
-                        return el.id !== action.id;
-                    })
-                ]
-            };
+            return deleteResult(state, action);
 
         default:
             return state;
     }
+};
+
+const storeResult = (state, action) => {
+    return updateObject(state, {
+        results: [...state.results, {
+            id: new Date(),
+            value: action.result
+        }]
+    });
+};
+
+const deleteResult = (state, action) => {
+    return updateObject(state, {
+        results: [...state.results.filter(el => {
+            return el.id !== action.id;
+        })]
+    });
 };
 
 export default reducer;
